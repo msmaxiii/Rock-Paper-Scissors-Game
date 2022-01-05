523b90c190bff0a61cb7e37e75a3b8298687aae4
Rochelle Maxwell <90586508+msmaxiii@users.noreply.github.com>
2022-01-05 01:51:57 -0500

    add style

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 let sign = prompt("What's your name?");
 
+const main = ()=>{
+    const div1 = document.getElementById('computer-choice')
+    const div2 = document.getElementById('player-choice')
+    const div3 = document.getElementById('result')
+    const randomChoice = document.querySelector('button')
+}
 
+let userChoice = userChoicepick.addEventListener('click',e)
+
+const displayRock = ()=>{
+    div.append('Rock')
+}
+const displaypaper = ()=>{
+    div.append('Paper')
+}
+
+div.append('Scissors');
+setTimeout(displayPaper,1000)
+setTimeout(displayScissors,2000)
+setTimeout(displayShoot,3000)
 
 
 

b1b638bd47186eec76309b7840d21d82c5302134
Rochelle Maxwell <90586508+msmaxiii@users.noreply.github.com>
2022-01-04 23:58:51 -0500

    add div

---
+++ app.js
@@ -0,0 +1,39 @@
+let sign = prompt("What's your name?");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+for (let i=0; i<;)
+
+
+
+
+
+
+
+
+function computer (){
+    const randomNumber = Math.floor(Math.random()*3) +1
+
+    if(randomNumber === 1) {
+        computerChoice ='rock'
+    }
+    if(randomNumber === 2) {
+        computerChoice ='paper'
+    } 
+    if(randomNumber === 3) {
+        computerChoice ='scissors'
+    }
+    comp
+}
